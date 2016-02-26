d9fc7fca63d50c535dde60d43b7b2f0c389193a0
Teixeira <teixeira.alexandre.dev.info@gmail.com>
2016-02-26 09:12:02 +0100

    Ajoue des recette

--- MVC/controlers/controlerRecherche.php
+++ MVC/controlers/controlerRecherche.php
@@ -11,7 +11,16 @@ class controlerRecherche extends Controler
 	}
 
 	function result(){
-		print_r($_POST);
+		$array_recette = array();
+
+		array_push($array_recette, $_POST['name']);
+		array_push($array_recette, $_POST['tmp_min']);
+		array_push($array_recette, $_POST['tmp_min']);
+		array_push($array_recette, $_POST['pseudo']);
+
+
+		$this->loadModel('modelRecherche');
+		$result = $this->modelRecherche->Recherche($array_recette);
 	}
 }
 ?>
--- MVC/models/modelRecherche.php
+++ MVC/models/modelRecherche.php
@@ -6,6 +6,21 @@
 class modelRecherche
 {
 	
-	
+	function Recherche($array)
+	{
+		print_r($array);
+		$sql = "SELECT id FROM Recette WHERE ";
+
+		if(!empty($array[0]))
+			$sql .= "nom LIKE '%".$array[0]."%' ";
+		if(!empty($array[1]))
+			$sql .= "temps > '".$array[1]."' ";
+		if(!empty($array[2]))
+			$sql .= "temps < '".$array[2]."' ";
+		if(!empty($array[3]))
+			$sql .= "pseudo LIKE '%".$array[3]."%'";
+
+		echo $sql;
+	}
 }
 ?>
